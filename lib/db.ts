f08ae740f8344657abe9d6f2e56cac79ac3aafc2
Leland Tran <leland@momos.io>
2025-03-11 14:38:04 +0800

    Set db rejectUnauthorized to false

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,3 @@
-
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
@@ -8,6 +7,9 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export const db = drizzle(pool, { schema });
